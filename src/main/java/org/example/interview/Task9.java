64133676eb385111f7985d72fc5a59589a4e55c3
SVEND-1 <s5090@inbox.ru>
2026-01-17 12:27:50 +0300

    .

---
+++ src/main/java/org/example/interview/Task9.java
@@ -0,0 +1,8 @@
+package org.example.interview;
+
+public class Task9 {
+    //– Напишите метод, который принимает строку и возвращает карту (Map), где ключом является символ, а значением — количество его вхождений в строку.
+    public static void main(String[] args) {
+
+    }
+}
