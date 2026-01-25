e50df0a7be166fc0759596cc2c0e1fcd38c89f7f
SVEND-1 <s5090@inbox.ru>
2026-01-25 22:37:45 +0300

    .

---
+++ src/main/java/org/example/testing/Task.java
@@ -0,0 +1,12 @@
+package org.example.testing;
+
+import java.util.Scanner;
+
+public class Task {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in); 
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a + b);
+    }
+}
